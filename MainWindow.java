cdcd86eda1545b785f309f1261a3fb5e0d3cf2bb
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-05 09:05:02 -0700

    Create methods to set generation and alive number

--- MainWindow.java
+++ MainWindow.java
@@ -1,27 +1,41 @@
 package life;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
+    final Font font = new Font("Dialog", Font.PLAIN, 16);
+
     private JLabel generationCount = new JLabel();
     private JLabel aliveCount = new JLabel();
 
     public MainWindow() {
         super("Game of Life");
         setName("Game of Life");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 500);
         setLocationRelativeTo(null);
 
-        generationCount.setBounds(0,0, 100, 30);
+        generationCount.setBounds(5,2, 150, 15);
+        generationCount.setFont(font);
         generationCount.setText("Generation #xxx");
         add(generationCount);
 
-        aliveCount.setBounds(0,20,100, 30);
+        aliveCount.setBounds(5,19,150, 15);
+        aliveCount.setFont(font);
         aliveCount.setText("Alive: xxx");
         add(aliveCount);
 
         setLayout(null);
         setVisible(true);
+    }
+
+    public void setGenerationNumber(int generationNumber) {
+        generationCount.setText("Generation #" + generationNumber);
+    }
+
+    public void setAliveNumber(int aliveNumber) {
+        aliveCount.setText("Alive: " + aliveNumber);
     }
 
     public static void main(String[] args) {
