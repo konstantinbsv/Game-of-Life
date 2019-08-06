9a48eb50709dda649f946cc728d39a5835b70276
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-05 17:46:19 -0700

    Create subclass SquareGrid which extends JPanel

--- MainWindow.java
+++ MainWindow.java
@@ -5,26 +5,34 @@ import java.awt.*;
 
 public class MainWindow extends JFrame {
     final Font font = new Font("Dialog", Font.PLAIN, 16);
+    final int labelWidth = 150;
+    final int labelHeight = 15;
+    final int xOffset = 5;
 
     private JLabel generationCount = new JLabel();
     private JLabel aliveCount = new JLabel();
+    private SquareGrid grid;
 
-    public MainWindow() {
+    public MainWindow(int gridSize) {
         super("Game of Life");
         setName("Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 500);
         setLocationRelativeTo(null);
 
-        generationCount.setBounds(5,2, 150, 15);
+        generationCount.setBounds(xOffset,2, labelWidth, labelHeight);
         generationCount.setFont(font);
         generationCount.setText("Generation #xxx");
         add(generationCount);
 
-        aliveCount.setBounds(5,19,150, 15);
+        aliveCount.setBounds(xOffset,19, labelWidth, labelHeight);
         aliveCount.setFont(font);
         aliveCount.setText("Alive: xxx");
         add(aliveCount);
+
+        grid = new SquareGrid(gridSize);
+        add(grid);
+        grid.repaint();
 
         setLayout(null);
         setVisible(true);
@@ -38,7 +46,50 @@ public class MainWindow extends JFrame {
         aliveCount.setText("Alive: " + aliveNumber);
     }
 
+    public void updateGrid(boolean[][] universeGrid) {
+        grid.setUniverseGrid(universeGrid);
+        add(grid);
+        grid.repaint();
+    }
+
+    private class SquareGrid extends JPanel {
+        final int squareSize = 15;
+
+        private int gridSize;
+        private boolean[][] universeGrid;
+
+        private SquareGrid(int gridSize) {
+            this.gridSize = gridSize;
+            setBounds(xOffset, 40, squareSize*gridSize, squareSize*gridSize);
+            setLayout(new GridLayout(gridSize, gridSize));
+        }
+
+        private void setUniverseGrid(boolean[][] universeGrid) {
+            this.universeGrid = universeGrid;
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.BLACK);
+
+            for (int i = 0; i < gridSize; i++) {
+                for (int j = 0; j < gridSize; j++) {
+                    if (universeGrid == null) {
+                        g.drawRect( j*squareSize,i*squareSize ,squareSize, squareSize);
+                    } else if (universeGrid[i][j]) {
+                        g.fillRect( j*squareSize,i*squareSize ,squareSize, squareSize);
+                    } else {
+                        g.drawRect( j*squareSize,i*squareSize ,squareSize, squareSize);
+                    }
+                }
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow();
+        MainWindow mainWindow = new MainWindow(40);
+
     }
 }
