457a0d9a696ea9f2901d1daa7dd97ed63cee81ba
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-07 14:03:26 -0700

    Add slider to set universe size and use subpixel precision for squares, calculate size based on screen size

--- GameOfLife.java
+++ GameOfLife.java
@@ -6,6 +6,8 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.util.Hashtable;
 
 public class GameOfLife extends JFrame {
     final Font font = new Font("Dialog", Font.PLAIN, 16);
@@ -22,16 +24,22 @@ public class GameOfLife extends JFrame {
     private JToggleButton startStopButton = new JToggleButton();
     private JButton resetButton = new JButton();
     private JSlider speedSlider = new JSlider(100, 500, 250);
-    private SquareGrid grid;
+    private JLabel speedSliderLabel = new JLabel();
+    private JSlider universeSizeSlider = new JSlider(10, 200, 50);
+    private JLabel sizeSliderLabel = new JLabel();
+    private static SquareGrid grid;
 
-    private boolean isPaused = false;
+    Hashtable<Integer, JLabel> sizeSliderTickLabels = new Hashtable<>();
+
+    private boolean isPaused = true;
+    private boolean isReset = true;
     private int evolutionSpeed;
+    private int universeSize;
 
-    public GameOfLife(int gridSize) {
+    public GameOfLife() {
         super("Game of Life");
         setName("Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(xOffset + leftMargin*2 + SquareGrid.squareSize*gridSize,  50 + gridSize*SquareGrid.squareSize);
         setLocationRelativeTo(null);
 
         generationCount.setBounds(leftMargin, topMargin, labelWidth, labelHeight);
@@ -48,12 +56,25 @@ public class GameOfLife extends JFrame {
 
         startStopButton.setBounds(leftMargin, topMargin + 40, buttonWidth, buttonHeight);
         startStopButton.setName("PlayToggleButton");
-        startStopButton.setText("Start/Stop");
-        // startStopButton.setIcon();
+        startStopButton.setText("Start");
+        isReset = true;
         startStopButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                isPaused = !isPaused;
+                if (isReset) {
+                    universeSizeSlider.setEnabled(false);
+                    initializeGrid(universeSize);
+                    isReset = false;
+                }
+
+                if (startStopButton.isSelected()) {
+                    startStopButton.setText("Pause");
+                    isPaused = false;
+                } else {
+                    startStopButton.setText("Start");
+                    isPaused = true;
+                }
+
             }
         });
         add(startStopButton);
@@ -64,7 +85,12 @@ public class GameOfLife extends JFrame {
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Universe.resetUniverse();
+                startStopButton.setSelected(false);
+                startStopButton.setText("Start");
+                isReset = true;
+                isPaused = true;
+
+                universeSizeSlider.setEnabled(true);
             }
         });
         add(resetButton);
@@ -81,17 +107,61 @@ public class GameOfLife extends JFrame {
             @Override
             public void stateChanged(ChangeEvent e) {
                 evolutionSpeed = speedSlider.getValue();
+                updateSpeedSliderLabel();
             }
         });
         add(speedSlider);
 
-        grid = new SquareGrid(gridSize);
-        add(grid);
-        grid.repaint();
+        speedSliderLabel.setBounds(leftMargin + 10, topMargin + 140, buttonWidth + 30, buttonHeight+20);
+        speedSliderLabel.setFont(font.deriveFont(9.0f));
+        updateSpeedSliderLabel();
+        add(speedSliderLabel);
+
+        universeSizeSlider.setBounds(leftMargin , topMargin + 180, buttonWidth + 30, buttonHeight+20);
+        universeSizeSlider.setToolTipText("Choose universe size");
+        sizeSliderTickLabels.put(10, new JLabel("10"));
+        sizeSliderTickLabels.put(50, new JLabel("50"));
+        sizeSliderTickLabels.put(100, new JLabel("100"));
+        sizeSliderTickLabels.put(150, new JLabel("150"));
+        sizeSliderTickLabels.put(200, new JLabel("200"));
+        universeSizeSlider.setSnapToTicks(true);
+        universeSizeSlider.setLabelTable(sizeSliderTickLabels);
+        universeSizeSlider.setPaintLabels(true);
+        universeSizeSlider.setPaintTicks(true);
+        universeSize = universeSizeSlider.getValue();
+        universeSizeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                universeSize = universeSizeSlider.getValue();
+                sizeSliderLabel.setText("Universe size: " + universeSize);
+            }
+        });
+        add(universeSizeSlider);
+
+        sizeSliderLabel.setBounds(leftMargin + 30, topMargin + 210, buttonWidth + 30, buttonHeight+20);
+        sizeSliderLabel.setText("Universe size: " + universeSize);
+        sizeSliderLabel.setFont(font.deriveFont(9.0f));
+        add(sizeSliderLabel);
 
         setLayout(null);
+        setSize(xOffset,  300);
         // setResizable(false);
         setVisible(true);
+    }
+
+    private void initializeGrid(int universeSize) {
+        if (grid != null) {
+            remove(grid);
+            grid = null;
+        }
+
+        grid = new SquareGrid(universeSize);
+        setSize(xOffset + leftMargin*2 + grid.gridPixelDimensions,  50 + grid.gridPixelDimensions);
+        setLocationRelativeTo(null);
+    }
+
+    private void updateSpeedSliderLabel() {
+        speedSliderLabel.setText("Generation delay: " + evolutionSpeed + "ms");
     }
 
     public void setGenerationNumber(int generationNumber) {
@@ -116,15 +186,29 @@ public class GameOfLife extends JFrame {
         return evolutionSpeed;
     }
 
-    private class SquareGrid extends JPanel {
-        final static int squareSize = 15;
+    public int getUniverseSize() {
+        return universeSize;
+    }
 
-        private int gridSize;
+    public boolean isReset() {
+        return isReset;
+    }
+
+    public void setReset(boolean reset) {
+        isReset = reset;
+    }
+
+    private class SquareGrid extends JPanel {
+        private double squareSize;
+
+        private int gridPixelDimensions;
         private boolean[][] universeGrid;
 
         private SquareGrid(int gridSize) {
-            this.gridSize = gridSize;
-            setBounds(xOffset, topMargin, squareSize*gridSize, squareSize*gridSize);
+            this.squareSize = (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 100) / gridSize;
+            this.gridPixelDimensions = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 100);
+
+            setBounds(xOffset, topMargin, gridPixelDimensions,  gridPixelDimensions);
             setLayout(new GridLayout(gridSize, gridSize));
         }
 
@@ -134,26 +218,28 @@ public class GameOfLife extends JFrame {
 
         @Override
         public void paintComponent(Graphics g) {
-            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D) g;
+            super.paintComponent(g2);
             g.setColor(Color.BLACK);
 
-            for (int i = 0; i < gridSize; i++) {
-                for (int j = 0; j < gridSize; j++) {
-                    if (universeGrid == null) {
-                        g.drawRect( j*squareSize,i*squareSize ,squareSize, squareSize);
-                    } else if (universeGrid[i][j]) {
-                        g.fillRect( j*squareSize,i*squareSize ,squareSize, squareSize);
+            for (int i = 0; i < universeGrid.length; i++) {
+                for (int j = 0; j < universeGrid.length; j++) {
+                    Rectangle2D rect = new Rectangle2D.Double(j * squareSize, i * squareSize, squareSize, squareSize);
+
+                    if (universeGrid == null || !universeGrid[i][j]) {
+                        g2.draw(rect);
+                        // g2.drawRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     } else {
-                        g.drawRect( j*squareSize,i*squareSize ,squareSize, squareSize);
+                        g2.fill(rect);
+                        // g2.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     }
                 }
             }
-
         }
     }
 
     public static void main(String[] args) {
-        GameOfLife mainWindow = new GameOfLife(40);
+        GameOfLife mainWindow = new GameOfLife();
 
     }
 }
